875c8085ac65290f75a6c7541496e66bb99bf581
96a86c <96a86c@googlegroups.com>
2019-05-10 21:08:02 +0900

    chore: fix initialize wait

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -2,14 +2,37 @@ import * as React from 'react'
 const { useEffect } = React
 import { useObserver } from 'mobx-react-lite'
 
+import { Switch, Route } from 'react-router'
+import { Link } from 'react-router-dom'
+
 import appStore from '../stores/app'
 import Layout from '../presenters/Layout'
+
+import logo from '../static/logo.png'
 
 export default () => {
   useEffect(() => {
     appStore.init()
   }, [])
   return useObserver(() => {
-    return <Layout me={appStore.me} />
+    if (!appStore.initialized)
+      return (
+        <div className="mozuku-layout">
+          <div className="mozuku-header-wrapper">
+            <div className="mozuku-header">
+              <h1 className="mozuku-header__logo">
+                <Link to={{ pathname: '/' }}>
+                  <img src={logo} width="64" height="64" alt="Mozuku" />
+                </Link>
+              </h1>
+            </div>
+          </div>
+        </div>
+      )
+    return (
+      <>
+        <Layout me={appStore.me} />
+      </>
+    )
   })
 }
