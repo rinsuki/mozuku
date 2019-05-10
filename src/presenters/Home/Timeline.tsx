eed2b2673dc71400b0f3bd992939e72ece7e1746
96a86c <96a86c@googlegroups.com>
2019-05-10 20:43:55 +0900

    chore: add placeholder to timeline

--- src/presenters/Home/Timeline.tsx
+++ src/presenters/Home/Timeline.tsx
@@ -13,6 +13,37 @@ export default ({
   readMoreDisabled: boolean
 }) => (
   <ul className="timeline">
+    {Array.from(Array(Number(!timeline.length) * 10), (v, k) => k).map(post => {
+      return (
+        <li className="timelineItem" key={post}>
+          <div className="ph-item">
+            <div className="ph-col-4">
+              <div className="ph-row">
+                <div className="ph-col-6" />
+                <div className="ph-col-2 empty" />
+                <div className="ph-col-4" />
+              </div>
+            </div>
+            <div className="ph-col-8">
+              <div className="ph-row">
+                <div className="ph-col-8 empty" />
+                <div className="ph-col-4" />
+              </div>
+            </div>
+            <div className="ph-col-12">
+              <div className="ph-row">
+                <div className="ph-col-12 big" />
+              </div>
+            </div>
+            <div className="ph-col-4">
+              <div className="ph-row">
+                <div className="ph-col-12" />
+              </div>
+            </div>
+          </div>
+        </li>
+      )
+    })}
     {timeline.map(post => (
       <li className="timelineItem" key={post.id}>
         <Post post={post} />
