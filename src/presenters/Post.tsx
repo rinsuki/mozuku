b5f1c48f0a706efb4facc4752ceae4ffff2b34d6
96a86c <96a86c@googlegroups.com>
2019-05-11 17:59:32 +0900

    chore: broken impl

--- src/presenters/Post.tsx
+++ src/presenters/Post.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import moment from 'moment-timezone'
+import axios from 'axios'
+import Config from '../config'
 
 import {
   Post,
@@ -54,6 +56,25 @@ export default ({ post }: { post: Post }) => (
               </div>
             </a>
           )}
+          {p.payload.startsWith('https://i.imgur.com') &&
+          localStorage.getItem(
+            `imgur_${p.payload.split('com/')[1].split('.')[0]}`
+          ) ? (
+            <button
+              onClick={axios
+                .delete(
+                  `/imgur/image/${p.payload.split('com/')[1].split('.')[0]}`,
+                  {
+                    headers: {
+                      Authorization: `Client-ID ${Config.imgur_client_id}`
+                    }
+                  }
+                )
+                .then(resp => console.log('deleted'))}
+            >
+              delete
+            </button>
+          ) : null}
         </React.Fragment>
       ))}
     </div>
